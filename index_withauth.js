21613017657527ce5e160711633d33a8146cf4ed
Sierra Ripoche <ripochesierra@gmail.com>
2024-11-13 11:43:03 +0100

    Added comments to explain authentication code in index_withauth.js

--- index_withauth.js
+++ index_withauth.js
@@ -7,9 +7,25 @@ const app = express();
 const PORT = 5000;
 
 // Initialize session middleware with options
+/* This tells the express app to use the session middleware
+- secret - a random unique string key used to authenticate a session
+- resave - takes a boolean value and enables the session to be stored back to the session store,
+            even if the session was never modified during the request
+- saveUninitialized - allows any uninitialized session to be stored back to the session store.
+                    When a session is created but not modified, it is referred to as uninitialized.
+
+
+Default values for 'resave' and 'saveUninitialized' are true, but the default is deprecated.
+So, set the appropriate value according to the use case. */
+
 app.use(session({ secret: "fingerpint", resave: true, saveUninitialized: true }));
 
 // Middleware for user authentication
+/* Implementation of the Authentiation middleware where all the endpoints starting with /user go through.
+It will retrieve the authorization details from the session and verify it.
+IF the token is validated, the user is authenticated and control is passed on to the next endpoint handler.
+If the token is invalid, the user is not authenticated and an error message is returned.
+*/
 app.use("/user", (req, res, next) => {
     // Check if user is authenticated
     if (req.session.authorization) {
@@ -38,6 +54,12 @@ app.use(express.json());
 app.use("/user", routes);
 
 // Login endpoint
+/* 
+1) A user logs into the system providing a username.
+2) An access token that is valid for one hour is generated. (60 * 60 signifies the time in seconds)
+3) This access token is set into the session object  to ensure that only authenticated users can access the endpoints for  1 hour
+*/
+
 app.post("/login", (req, res) => {
     const user = req.body.user;
     if (!user) {
